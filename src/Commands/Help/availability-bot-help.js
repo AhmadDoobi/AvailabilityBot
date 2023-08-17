a9854574d3e196e59a7e1878dd7f0b600db3c0db
AhmadDoobi <ahmad_doobi@icloud.com>
2023-08-17 16:08:35 +0300

    finished seeting the help command

---
+++ src/Commands/Help/availability-bot-help.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require("discord.js")
+const { helpEmbed } = require('../../Embeds/availability-bot-help-embed')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('availability-bot-help')
+        .setDescription('helps you learn how to use the bot'),
+    
+    async execute(interaction) {
+        const embed = await helpEmbed();
+        await interaction.reply({ embeds: [embed] });
+    }
+}
