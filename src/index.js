0c70daf89957e1c714b67b677f44980a1f596b59
AhmadDoobi <ahmad_doobi@icloud.com>
2023-08-17 16:30:55 +0300

    added the schedule function call to the index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ dotenv.config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const token = process.env.TOKEN;
 const { loadEvents } = require("./Handlers/event-handler");
-const { setupDatabase } = require('./Handlers/setup-db')
+const { setupDatabase } = require('./Functions/setup-db')
+const { setupSchedules } = require('./Handlers/schedule')
 // set the intetns for the bot
 const client = new Client({ intents: [
     GatewayIntentBits.DirectMessages,
@@ -21,6 +22,7 @@ client.events = new Collection();
 client.commands = new Collection();
 
 loadEvents(client);
+setupSchedules(client);
 
 // running the bot
 client.login(token);
