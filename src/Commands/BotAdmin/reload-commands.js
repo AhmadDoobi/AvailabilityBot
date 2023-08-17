6d05b8bcd481d27f45995952325a5636fe0320e3
AhmadDoobi <ahmad_doobi@icloud.com>
2023-08-17 16:07:09 +0300

    fixed the problem with reloading the commands also removed reloading a specific command due to not working

--- src/Commands/BotAdmin/reload-commands.js
+++ src/Commands/BotAdmin/reload-commands.js
@@ -1,98 +1,23 @@
 const { SlashCommandBuilder, PermissionFlagsBits} = require('discord.js');
-const { loadCommands } = require('../../Handlers/command-handler');
-const { reloadSpecificCommand } = require('../../Handlers/reload-specific-command')
-
+const { loadCommands } = require('../../Handlers/command-handler')
+const {  } = require('../../Handlers/reload-teams-games-commands')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('reload-commands')
-		.setDescription('Reloads a command, global commands, or all commands.')
+		.setDescription('Reloads all commands, commands including team choices, or commands including games choices.')
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 		.addSubcommand(subcommand => 
 			subcommand 
 				.setName('all-commands')
 				.setDescription('reload all commands'))
-		.addSubcommandGroup(subcommandgroup =>
-			subcommandgroup
-				.setName('specific')
-				.setDescription('reload a specific command')
-				.addSubcommand(subcommand =>
-					subcommand
-						.setName('bot_admin')
-						.setDescription('BotAdmin category')
-						.addStringOption(option =>
-							option 
-								.setName('command-name')
-								.setDescription('select the command name')
-								.setRequired(true)
-								.addChoices(
-									{name: 'delete-team', value: 'delete-team'},
-									{name: 'games-file', value: 'games-file'},
-									{name: 'reload-commands', value: 'reload-commands'},
-								)
-						)
-				)
-				.addSubcommand(subcommand =>
-					subcommand
-						.setName('help')
-						.setDescription('Help category')
-						.addStringOption(option =>
-							option 
-								.setName('command-name')
-								.setDescription('select the command name')
-								.setRequired(true)
-								.addChoices(
-									{name: 'help', value: 'help'},
-								)
-						)
-				
-				)
-				.addSubcommand(subcommand =>
-					subcommand
-						.setName('match_setup')
-						.setDescription('MatchSetup category')
-						.addStringOption(option =>
-							option 
-								.setName('command-name')
-								.setDescription('select the command name')
-								.setRequired(true)
-								.addChoices(
-									{name: 'match-setup', value: 'match-setup'},
-								)			
-						)
-				)
-				.addSubcommand(subcommand =>
-					subcommand
-						.setName('moderation')
-						.setDescription('Moderation category')
-						.addStringOption(option =>
-							option 
-								.setName('command-name')
-								.setDescription('select the command name')
-								.setRequired(true)
-								.addChoices(
-									{name: 'register-team', value: 'register-team'},
-									{name: 'reset-team-info', value: 'reset-team-info'},
-									{name: 'set-times', value: 'set-times'},
-									{name: 'reset-times', value: 'reset-times'},
-								)			
-						)
-				)
-				.addSubcommand(subcommand =>
-					subcommand
-						.setName('teams_info')
-						.setDescription('TeamsInfo category')
-						.addStringOption(option =>
-							option 
-								.setName('command-name')
-								.setDescription('select the command name')
-								.setRequired(true)
-								.addChoices(
-									{name: 'get-team-info', value: 'get-team-info'},
-									{name: 'teams-availability', value: 'teams-availability'},
-								)			
-						)
-				)
-			),
+		.addSubcommand(subcommand => 
+			subcommand 
+				.setName('teams-commands')
+				.setDescription('reload commands that include teams choices'))
+		.addSubcommand(subcommand => 
+			subcommand 
+				.setName('games-commands')
+				.setDescription('reload commands that include games choices')),
 		async execute(interaction, client) {
 			await interaction.reply({
 				content: 'processing...',
@@ -101,14 +26,6 @@ module.exports = {
 
 			const subcommand = interaction.options.getSubcommand();
 
-			if (interaction.options.getSubcommandGroup()){
-				const commandName = interaction.options.getString('command-name');
-				const state = await reloadSpecificCommand(subcommand, commandName, client)
-				await interaction.editReply({
-					content: state,
-					ephemeral: true
-				})
-			}
 
 			if (subcommand === 'all-commands'){
 				const startup = false
