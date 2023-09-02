0452c87725c6967f4bdcd024f6b41a14ef1a8ad0
AhmadDoobi <ahmad_doobi@icloud.com>
2023-09-02 17:54:47 +0300

    added an admin command to cache times messages for use when the bot restarts

---
+++ src/Commands/BotAdmin/cache-times-messages.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { cacheMessages } = require('../../Functions/cache-messages');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cache-times-messages')
+        .setDescription('cache the times messages to get reactions')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction, client){
+        await interaction.reply({
+            content: 'processing...',
+            ephemeral: true
+        })
+        try {
+            await cacheMessages(client);
+            await interaction.editReply({
+                content: 'Successfully cached all times messages ',
+                ephemeral: true
+            })
+        } catch (error) {
+            console.log(`something went wrong while caching times messages on command: ${error}`)
+            await interaction.editReply({
+                content: 'something went wrong',
+                ephemeral: true
+            })
+        }
+    }
+}
