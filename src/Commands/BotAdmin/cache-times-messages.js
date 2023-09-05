7ba87530f450b65f07e6c3af556f46e0b89cb4c0
AhmadDoobi <ahmad_doobi@icloud.com>
2023-09-05 03:36:24 +0300

    added a command to cache times messages

--- src/Commands/BotAdmin/cache-times-messages.js
+++ src/Commands/BotAdmin/cache-times-messages.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { cacheMessages } = require('../../Functions/cache-messages');
 
 module.exports = {
